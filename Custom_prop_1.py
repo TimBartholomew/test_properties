080afaeb0fb85141fdecbacca42a6a0db7bc2f42
Tim Bartholomew <barthotv@AD.SCILAN.PGH>
2020-02-11 16:07:19 -0500

    Basic prop package, not ran

--- Custom_prop_1.py
+++ Custom_prop_1.py
@@ -6,7 +6,9 @@ Initial property package for H2O-NaCl system
 import logging
 
 # Import Pyomo libraries
-# from pyomo.environ import Param, NonNegativeReals, Set
+from pyomo.environ import Constraint, Expression, log, NonNegativeReals,\
+    Var, Set, Param, sqrt, log10
+from pyomo.opt import SolverFactory
 
 # Import IDAES cores
 from idaes.core import (declare_process_block_class,
@@ -16,43 +18,199 @@ from idaes.core import (declare_process_block_class,
                         StateBlock,
                         MaterialBalanceType,
                         EnergyBalanceType)
-# from idaes.core.util.misc import extract_data
-
-# Some more information about this module
-__author__ = "Tim Bartholomew"
-__version__ = "0.0.1"
+from idaes.core.util.initialization import (fix_state_vars,
+                                            revert_state_vars,
+                                            solve_indexed_blocks)
+from idaes.core.util.misc import add_object_reference, extract_data
+from idaes.core.util.model_statistics import degrees_of_freedom, \
+                                             number_unfixed_variables
 
 # Set up logger
 _log = logging.getLogger(__name__)
 
 
-@declare_process_block_class("HDAParameterBlock")
-class HDAParameterData(PhysicalParameterBlock):
+@declare_process_block_class("NaClParameterBlock")
+class NaClParameterData(PhysicalParameterBlock):
     CONFIG = PhysicalParameterBlock.CONFIG()
 
     def build(self):
         '''
         Callable method for Block construction.
         '''
-        super(HDAParameterData, self).build()
+        super(NaClParameterData, self).build()
 
         self.state_block_class = IdealStateBlock
 
-        self.component_list = Set(initialize=['H2O',
-                                              'NaCl'])
+        self.component_list = Set(initialize=['H2O','NaCl'])
 
-        self.phase_list = Set(initialize=['Liq', 'Vap'],
-                              ordered=True)
+        # self.phase_list = Set(initialize=['Liq'],ordered=True)
 
         # List of components in each phase (optional)
-        self.phase_comp = {"Liq": self.component_list,
-                           "Vap": self.component_list}
+        # self.phase_comp = {"Liq": self.component_list}
 
         # Source: google
-        mw_comp_data = {'H2O': 18.0E-3,
-                        'NaCl': 58.4E-3}
+        mw_comp_data = {'H2O': 18.0E-3,'NaCl': 58.4E-3}
 
         self.mw_comp = Param(self.component_list,
                              mutable=False,
                              initialize=extract_data(mw_comp_data),
                              doc="molecular weight Kg/mol")
+
+    @classmethod
+    def define_metadata(cls, obj):
+        """Define properties supported and units."""
+        obj.add_properties(
+            {'flow_mass': {'method': None, 'units': 'g/s'},
+             'flow_mass_comp': {'method': None, 'units': 'g/s'},
+             'mass_frac': {'method': None, 'units': 'none'},
+             'temperature': {'method': None, 'units': 'K'},
+             'pressure': {'method': None, 'units': 'Pa'}})
+
+        obj.add_default_units({'time': 's',
+                               'length': 'm',
+                               'mass': 'g',
+                               'amount': 'mol',
+                               'temperature': 'K',
+                               'energy': 'J',
+                               'holdup': 'g'})
+
+
+class _IdealStateBlock(StateBlock):
+    """
+    This Class contains methods which should be applied to Property Blocks as a
+    whole, rather than individual elements of indexed Property Blocks.
+    """
+
+    def initialize(blk, state_args={}, state_vars_fixed=False,
+                   hold_state=False, outlvl=1,
+                   solver='ipopt', optarg={'tol': 1e-8}):
+        """
+        Initialization routine for property package.
+        Keyword Arguments:
+            state_args : Dictionary with initial guesses for the state vars
+                         chosen. Note that if this method is triggered
+                         through the control volume, and if initial guesses
+                         were not provied at the unit model level, the
+                         control volume passes the inlet values as initial
+                         guess.The keys for the state_args dictionary are:
+
+                         flow_mol_phase_comp : value at which to initialize
+                                               phase component flows
+                         pressure : value at which to initialize pressure
+                         temperature : value at which to initialize temperature
+            outlvl : sets output level of initialization routine
+                     * 0 = no output (default)
+                     * 1 = return solver state for each step in routine
+                     * 2 = include solver output infomation (tee=True)
+            optarg : solver options dictionary object (default=None)
+            state_vars_fixed: Flag to denote if state vars have already been
+                              fixed.
+                              - True - states have already been fixed by the
+                                       control volume 1D. Control volume 0D
+                                       does not fix the state vars, so will
+                                       be False if this state block is used
+                                       with 0D blocks.
+                             - False - states have not been fixed. The state
+                                       block will deal with fixing/unfixing.
+            solver : str indicating whcih solver to use during
+                     initialization (default = 'ipopt')
+            hold_state : flag indicating whether the initialization routine
+                         should unfix any state variables fixed during
+                         initialization (default=False).
+                         - True - states varaibles are not unfixed, and
+                                 a dict of returned containing flags for
+                                 which states were fixed during
+                                 initialization.
+                        - False - state variables are unfixed after
+                                 initialization by calling the
+                                 relase_state method
+        Returns:
+            If hold_states is True, returns a dict containing flags for
+            which states were fixed during initialization.
+        """
+
+        _log.info('Starting {} initialization'.format(blk.name))
+
+        # Fix state variables if not already fixed
+        if state_vars_fixed is False:
+            flags = fix_state_vars(blk, state_args)
+
+        else:
+            # Check when the state vars are fixed already result in dof 0
+            for k in blk.keys():
+                if degrees_of_freedom(blk[k]) != 0:
+                    raise Exception("State vars fixed but degrees of freedom "
+                                    "for state block is not zero during "
+                                    "initialization.")
+        # Set solver options
+        if outlvl > 1:
+            stee = True
+        else:
+            stee = False
+
+        if optarg is None:
+            sopt = {'tol': 1e-8}
+        else:
+            sopt = optarg
+
+        opt = SolverFactory('ipopt')
+        opt.options = sopt
+
+@declare_process_block_class("IdealStateBlock",
+                             block_class=_IdealStateBlock)
+class IdealStateBlockData(StateBlockData):
+    """An example property package for ideal VLE."""
+
+    def build(self):
+        """Callable method for Block construction."""
+        super(IdealStateBlockData, self).build()
+
+        # Add state variables
+        # obj.add_properties(
+        #     {'flow_mass': {'method': None, 'units': 'g/s'},
+        #      'flow_mass_comp': {'method': None, 'units': 'g/s'},
+        #      'mass_frac': {'method': None, 'units': 'none'},
+        #      'temperature': {'method': None, 'units': 'K'},
+        #      'pressure': {'method': None, 'units': 'Pa'}})
+
+        self.flow_mass = Var(
+            initialize=0.5,
+            bounds=(1e-8, 100),
+            doc='mass flow rates [g/s]')
+
+        self.flow_mass_comp = Var(self._params.component_list,
+                                  initialize=0.5,
+                                  bounds=(1e-8, 100),
+                                  doc='mass flow rates [g/s]')
+
+        self.pressure = Var(initialize=101325,
+                            bounds=(101325, 400000),
+                            domain=NonNegativeReals,
+                            doc='State pressure [Pa]')
+
+        self.temperature = Var(initialize=298.15,
+                               bounds=(298.15, 1000),
+                               domain=NonNegativeReals,
+                               doc='State temperature [K]')
+
+        # Add supporting variables
+        def mass_frac(b):
+            return sum(b.flow_mass_comp[j]
+                       for j in b._params.component_list)
+        self.mass_frac = Expression(rule=mass_frac,
+                                    doc='mass fraction [unitless]')
+
+# -----------------------------------------------------------------------------
+# Property Methods
+#     def _dens_mol_phase(self):
+#         self.dens_mol_phase = Var(self._params.phase_list,
+#                                   initialize=1.0,
+#                                   doc="Molar density [mol/m^3]")
+# -----------------------------------------------------------------------------
+# General Methods
+#     def get_material_flow_terms(self, p, j):
+#         """Create material flow terms for control volume."""
+#         if j in self._params.component_list:
+#             return self.flow_mol_phase_comp[p, j]
+#         else:
+#             return 0
